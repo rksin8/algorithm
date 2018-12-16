cd667530c0e712a91bc0f8a8ccc525cf27b8869a
rksin8 <ranjeetbhu88@gmail.com>
2018-12-16 17:13:22 +1100

    firstPositionOfCharacter.cpp corrected

--- recursion/firstPositionOfCharacter.cpp
+++ recursion/firstPositionOfCharacter.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <string>
 
-int firstPost(std::string word, char c){
+int firstPost(std::string word, char c, int pos = 0){
     
     if(word.length()==0)
         return 0;
+    else if (word.length() == 1 && word[0] != c)
+        return -pos;
     else if (word[0]==c)
         return 1;
     else{
         auto newWord = word.substr(1, word.length()-1);
-        return 1 + firstPost(newWord, c);
+        return 1 + firstPost(newWord, c, pos+1);
     }
 }
 
@@ -20,6 +22,7 @@ int main(int argc, char *argv[])
     char c;
     std::cin >> word >> c;
 
+    std::cout << "Word length = " << word.length() << std::endl;
     std::cout << firstPost(word, c) << std::endl;
     return 0;
 }
